e23d7380b5440c37401477ac4809d30a2ee5b334
Bazina <60438793+Bazina@users.noreply.github.com>
2020-10-18 22:53:34 +0200

    git commit

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
-// Require Express to run server and routes
+/* Require Express to run server and routes, Dependencies */
+const express = require("express");
+const bodyParser = require("body-parser");
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +14,35 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+
+// Port number
+const port = 3000;
+
+// listens when the server is running
+app.listen(port, () => {
+    console.log(`Server is running on localhost ${port}`)
+});
+
+// Get Route
+app.get("/all", (req, res) => {
+    res.send(projectData);
+});
+
+// Post Route
+const data = [];
+app.post("/addData", addData);
+
+function addData(req, res) {
+    projectData.date = req.body.date;
+    projectData.temperature = req.body.main.temp;
+    projectData.feelings = req.body.content;
+    res.send(projectData);
+    console.log(projectData);
+}
--- website/app.js
+++ website/app.js
@@ -1,5 +1,90 @@
 /* Global Variables */
+const baseURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&zip=";
+const apiKey = "&appid=e96de8cd3cbefa2956325873b44a58e2";
 
-// Create a new date instance dynamically with JS
+/* Create a new date instance dynamically with JS
+* One was added beacause getMonth return months from 0 to 11
+*/
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = `${d.getMonth() + 1}-${d.getDate()}-${d.getFullYear()}`;
+
+// Have the button ready to generate responce on click
+const generate = document.getElementById("generate");
+generate.addEventListener("click", geneData);
+
+function geneData(e) {
+    if (zip.value===""){
+        alert("Enter your zip code");
+    } else {
+        e.preventDefault();
+        // Get user inputs
+        const feelings = document.getElementById("feelings").value;
+        const zip = document.getElementById("zip").value;
+        getWeather(baseURL,zip,apiKey)
+        // Post data to server
+        .then(function (data) {
+            postData("/addData", {date: newDate, temp: data.main.temp, content: feelings})
+        })
+        // Update UI data
+        .then(
+            updateUI()
+        )
+    }
+};
+
+// Get request data from API
+const getWeather = async(baseURL, zip, apiKey) => {
+    try {
+        const res = await fetch(
+            `${baseURL}${zip}${apiKey}`
+        );
+        // Save data in res as json
+        const data = res.json();
+        console.log(data);
+        return data;
+    } catch(error) {
+        console.log("Error", error);
+    }
+};
+
+// Get data from server
+const getData = async(url = "") => {
+    const res = await fetch(url);
+    try {
+        const data = await res.json();
+        console.log(data);
+        return(data)
+    } catch {
+        console.log("Error", error);
+    }
+};
+
+// Post data to server endpoint (projectData)
+const postData = async(url = "", data = {}) => {
+    const res = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        credentials: "same-origin",
+        body: JSON.stringify(data),
+    });
+    try {
+        const newData = await res.json();
+        console.log(newData);
+        return (newData);
+    } catch(error) {
+        console.log("Error", error);
+    }
+}
+
+const updateUI = async() => {
+    const projectData = await getData("/all");
+    try {
+        document.getElementById("date").innerHTML = `${projectData.date}`;
+        document.getElementById("temp").innerHTML = `${projectData.temperature}&#8451;`;
+        document.getElementById("content").innerHTML = `${projectData.feelings}`;
+    } catch {
+        console.log("Error", error);
+    }
+}
